9ab57b5ac05315f9367d29c09d8ad60b61f4c715
Lindenbrock <64817739+Lindenbrock@users.noreply.github.com>
2021-05-12 08:13:43 -0500

    Initial commit

---
+++ src/Figure.java
@@ -0,0 +1,210 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+class punto2D{
+	double x,y;
+	
+	public punto2D(double a, double b) {
+		x=a;y=b;
+	}
+}
+
+public class Figure {
+	punto2D vecFigures[],fig1[],figMap1[],fig2[],figMap2[],figaux[],figMapAux[];
+	int puntosf1[],puntosf2[];
+	Color uno = new Color(247,247,247);
+	Color tres = new Color(95,24,84);
+	
+	public Figure() {
+		fig1 = new punto2D[171];
+		puntosf1 = new int[] {
+				331,284, 154,46, 158,54, 156,63, 158,70, 158,79, 158,83, 161,89, 174,90, 179,89, 193,86, 196,86, 210,86, 220,92, 227,102, 229,115,
+				231,148, 239,152, 245,143, 259,145, 274,148, 279,151, 288,151, 288,142, 293,134, 291,116, 296,116, 313,106, 327,102, 340,99, 
+				353,97, 370,101, 391,99, 399,110, 396,121, 380,119, 368,123, 353,126, 354,142, 341,144, 338,152, 334,159, 353,160, 376,159, 
+				385,159, 386,152, 406,156, 419,166, 434,165, 435,174, 454,178, 460,191, 461,199, 457,214, 458,226, 430,223, 421,228, 421,236, 
+				431,240, 442,231, 453,232, 467,235, 469,247, 482,255, 502,260, 518,258, 527,264, 546,273, 566,269, 589,266, 598,265, 613,270, 
+				614,282, 627,291, 632,304, 640,319, 624,323, 631,336, 633,351, 629,357, 632,376, 623,381, 623,396, 617,408, 602,428, 590,424, 
+				577,427, 560,430, 545,436, 466,476, 435,476, 431,492, 418,520, 404,528, 392,536, 381,545, 356,553, 339,552, 321,545, 299,538, 
+				288,529, 279,516, 269,502, 264,488, 260,460, 261,448, 256,446, 245,446, 234,446, 224,444, 210,438, 202,431, 195,427, 184,424, 
+				174,418, 168,420, 163,428, 152,435, 146,441, 138,446, 127,449, 114,449, 103,449, 92,447, 78,445, 71,438, 65,431, 59,423, 
+				52,413, 51,402, 47,391, 43,380, 43,368, 43,359, 44,352, 48,335, 53,328, 38,326, 47,316, 54,298, 57,296, 77,292, 85,285, 102,276, 
+				116,271, 119,268, 128,256, 128,249, 135,239, 130,230, 127,219, 126,210, 129,199, 130,191, 130,180, 131,167, 131,160, 129,141, 
+				128,136, 123,126, 127,109, 128,102, 128,92, 131,73, 124,66, 140,60, 141,79, 143,90, 151,89, 154,46, 158,54
+
+		};
+		
+		//Incicializando la figura 1
+		for(int  pos=0,i=0,j=1;i<puntosf1.length-1;i+=2,pos++,j+=2)
+			fig1[pos]=new punto2D(puntosf1[i],puntosf1[j]);
+		
+		//Mapeo de la figura 1
+		figMap1 = new punto2D[fig1.length];
+		for(int i=0; i<figMap1.length; i++)
+			figMap1[i] = new punto2D(0,0);
+		
+		fig2 = new punto2D[169];
+		puntosf2 = new int[] {
+				225,198, 172,69, 176,63, 187,62, 197,63, 206,59, 211,59, 215,59, 222,55, 239,55, 240,67, 249,71, 263,71, 280,70, 283,62, 288,51, 301,51,
+				314,56, 322,55, 333,66, 347,83, 354,96, 358,105, 363,118, 374,125, 387,133, 394,140, 407,152, 414,160, 405,162, 395,160, 382,157, 
+				371,152, 351,147, 338,144, 330,142, 328,152, 330,168, 338,190, 352,188, 374,188, 373,191, 383,195, 392,199, 401,203, 407,206, 417,214, 
+				422,221, 428,225, 432,231, 437,238, 442,247, 443,255, 447,265, 450,278, 449,292, 449,305, 447,316, 442,334, 440,341, 434,356, 428,366, 
+				419,374, 412,380, 404,385, 398,389, 377,392, 361,392, 339,389, 325,383, 312,373, 305,357, 294,347, 288,328, 287,312, 283,292,
+				283,282, 276,273, 283,249, 292,229, 289,218, 278,214, 275,208, 269,208, 266,224, 270,234, 273,244, 273,255, 269,267, 263,274, 
+				253,282, 246,293, 233,306, 163,307, 154,305, 148,301, 142,302, 138,315, 132,329, 128,336, 122,346, 118,351, 112,359, 104,362, 
+				96,368, 89,368, 75,372, 61,370, 48,367, 38,361, 32,352, 27,342, 22,333, 15,320, 12,305, 13,291, 13,278, 16,262, 21,246, 29,236, 37,227, 
+				44,219, 56,210, 68,204, 44,188, 34,192, 26,206, 19,213, 11,215, 1,208, 9,192, 12,180, 6,180, 12,166, 18,155, 24,151, 
+				26,144, 12,139, 11,131, 24,125, 21,112, 37,112, 68,114, 79,122, 100,121, 116,121, 128,128, 138,131, 148,141, 161,142, 173,137, 
+				184,131, 185,130, 188,128, 193,120, 202,111, 216,105, 224,100, 240,95, 253,90, 243,77, 232,77, 218,81, 
+				206,81, 195,75, 187,75, 182,74, 172,69, 176,63
+		};
+		
+		//Incicializando la figura 2
+		for(int  pos=0,i=0,j=1;i<puntosf2.length-1;i+=2,pos++,j+=2)
+			fig2[pos]=new punto2D(puntosf2[i],puntosf2[j]);
+		
+		//Mapeo de la figura 1
+		figMap2 = new punto2D[fig2.length];
+		for(int i=0; i<figMap2.length; i++)
+			figMap2[i] = new punto2D(0,0);
+	}
+	
+	//MAPEO DE LAS FIGURAS CREADAS
+	public void windowMap(int xvmax, int xvmin, int yvmax, int yvmin, int xwmax, int ywmax, Graphics g,int nf) {
+		double sx = (double)(xvmax - xvmin) / (double)(xwmax - 0),  sy = (double)(yvmax - yvmin) / (double)(ywmax - 0);
+		
+		if(nf == 1) {
+			figaux = fig1; figMapAux = figMap1;
+		}else {
+			figaux = fig2; figMapAux = figMap2;
+		}
+		
+		g.setColor(uno);
+		for(int i=0;i<figaux.length;i++) {
+			figMapAux[i].x = figaux[i].x * sx;
+			figMapAux[i].y = figaux[i].y * sy;
+			figMapAux[i].x += xvmin;
+			figMapAux[i].y += yvmin;
+		}
+		for(int p1=1,p2=2;p1<figMapAux.length-1;p1+=1,p2+=1)
+			g.drawLine((int)figMapAux[p1].x, (int)figMapAux[p1].y, (int)figMapAux[p2].x, (int)figMapAux[p2].y);
+	}
+	
+	//DIBUJANDO LAS FIGURAS
+	public void DibujarVecPuntos(Graphics g, int nf) {
+		if(nf == 1) {
+			figaux = fig1; figMapAux = figMap1;
+		}else {
+			figaux = fig2; figMapAux = figMap2;
+		}
+		g.setColor(tres);
+		for(int p1=1,p2=2;p1<figaux.length-1;p1+=1,p2+=1) 
+			g.drawLine((int)figaux[p1].x,(int)figaux[p1].y,(int)figaux[p2].x,(int)figaux[p2].y);
+	}
+	
+	
+	/*-----  TRANSFORMACIONES PRINCIPALES DE LAS FIGURAS  -----*/
+	
+	//RESTAURAR FIGURA
+	public void restore(int nf) {
+		if(nf == 1) 
+			for(int  pos=0,i=0,j=1;i<puntosf1.length-1;i+=2,pos++,j+=2)
+				fig1[pos]=new punto2D(puntosf1[i],puntosf1[j]);
+		else
+			for(int  pos=0,i=0,j=1;i<puntosf2.length-1;i+=2,pos++,j+=2)
+				fig2[pos]=new punto2D(puntosf2[i],puntosf2[j]);
+	}
+	
+	
+	//ESCALAR FIGURA
+	public void scaleHPoint(double esc,int nf) {
+		// xSx - TxSx + Tx,ySy - TySy + Ty
+		if(nf == 1)
+			figaux = fig1;
+		else
+			figaux = fig2;
+		
+		int Tx = (int)figaux[0].x, Ty = (int)figaux[0].y;
+		for(punto2D fig : figaux) {
+			fig.x = fig.x * esc - Tx * esc + Tx;
+			fig.y = fig.y * esc - Ty * esc +Ty;
+		}
+	}
+	
+	//DEFORMAR FIGURA
+	public void shearyHPoint(double A, double B,int nf) {
+		//x + Ay - ATy, Bx + y - BTx, 1
+		if(nf == 1)
+			figaux = fig1;
+		else
+			figaux = fig2;
+		
+		int Tx = (int)figaux[0].x, Ty = (int)figaux[0].y;
+		for(punto2D fig : figaux) {
+			double x = fig.x;
+			fig.x = x + A*fig.y - A*Ty;
+			fig.y = B*x + fig.y - B*Tx;
+		}
+	}
+	
+	//ROTAR FIGURA A LA IZQUIERDA
+	public void rotateSinHPoint(int ang, int nf) {
+		//xcos - ysen - Txcos + Tysen + Tx, xsen +ycos - Txsen - Tycos + Ty
+		if(nf == 1)
+			figaux = fig1;
+		else
+			figaux = fig2;
+		
+		double angRad = Math.toRadians(ang), seno = Math.sin(angRad), coseno=Math.cos(angRad);
+		int Tx = (int)figaux[0].x, Ty = (int)figaux[0].y;
+		for(punto2D fig : figaux) {
+			double x = fig.x;
+			fig.x = x * coseno - fig.y * seno - Tx * coseno + Ty * seno + Tx;
+			fig.y = x * seno + fig.y * coseno - Tx * seno - Ty * coseno + Ty;
+		}
+	}
+	
+	public void rotateCosHPoint(int ang, int nf) {
+		//xcos + ysen - Txcos - Tysen + Tx, -xsen +ycos + Txsen - Tycos + Ty
+		if(nf == 1)
+			figaux = fig1;
+		else
+			figaux = fig2;
+		
+		double angRad = Math.toRadians(ang), seno = Math.sin(angRad), coseno=Math.cos(angRad);
+		int Tx = (int)figaux[0].x, Ty = (int)figaux[0].y;
+		for(punto2D fig : figaux) {
+			double x = fig.x;
+			fig.x = x * coseno + fig.y * seno - Tx * coseno - Ty * seno + Tx;
+			fig.y = -x * seno + fig.y * coseno + Tx * seno - Ty * coseno + Ty;
+		}
+	}
+	
+	//TRASLADAR FIGURA
+	public void movePoint(int tx, int ty, int nf) {
+		if(nf == 1)
+			figaux = fig1;
+		else
+			figaux = fig2;
+		
+		for(punto2D fig : figaux) {
+			fig.x += tx;
+			fig.y += ty;
+		}
+	}
+	
+	//REFLEJAR FIGURA
+	public void reflectHPoint(double refx, double refy, int nf) {
+		// xRx - TxRx + Tx, yRy - TyRy + Ty
+		if(nf == 1)
+			figaux = fig1;
+		else
+			figaux = fig2;
+		
+		int Tx = (int)figaux[0].x, Ty = (int)figaux[0].y;
+		for(punto2D fig : figaux) {
+			fig.x = fig.x * refx - Tx * refx + Tx;
+			fig.y = fig.y * refy - Ty * refy +Ty;
+		}
+		
+	}
+}
---
+++ src/MainInterface.java
@@ -0,0 +1,332 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.image.ImageObserver;
+import java.net.URL;
+import java.text.AttributedCharacterIterator;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+
+public class MainInterface extends JPanel{
+	JFrame W;
+	JPanel pestaña1,pestaña2;
+	JMenuBar menuBar;
+	JMenuItem menuOptions[]; //opcRestore,opcScale,opcSheary,opcReflection,opsTranstale,opcRotation,opcExit,opcAbout,opcHelp
+	JMenu menu1,menu2;
+	JToolBar toolBar;
+	String mOptions[];
+	Color uno,dos,tres,cuatro;
+	JTabbedPane pestañas;
+	URL ruta;
+	Figure fig1,fig2;
+	
+	public MainInterface() {
+		W = new JFrame("Transformaciones");
+		W.setSize(1000,600);
+		W.setLocationRelativeTo(this);
+		W.setResizable(false);
+		W.add(this);
+		
+		uno = new Color(247,247,247);
+		dos = new Color(59,9,68);
+		tres = new Color(95,24,84);
+		cuatro = new Color(26,187,156);
+		
+		buildMenu();
+		buildToolBar();
+		buildTabbedPane();
+		
+		fig1 = new Figure();
+		fig2 = new Figure();
+		
+		W.setVisible(true);
+		W.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	//CREANDO LAS PESTAÑAS QUE CONTENDRÁN LAS FIGURAS
+	private void buildTabbedPane() {
+		pestañas = new JTabbedPane();
+		pestañas.setBackground(cuatro);
+		W.add(pestañas);
+		
+		//Pintar figura en la pestaña 1
+		pestaña1 = new JPanel() {
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				int maxx=getWidth(),maxy=getHeight();
+				g.setColor(tres);
+				g.fillRect(maxx-300,maxy-200,300,200);
+				fig1.DibujarVecPuntos(g,1);
+				fig1.windowMap(maxx, maxx-300, maxy, maxy-200, maxx, maxy, g, 1);
+			}
+		};
+		
+		//Pintar figura en la pestaña 2
+		pestaña2 = new JPanel(){
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				int maxx=getWidth(),maxy=getHeight();
+				g.setColor(tres);
+				g.fillRect(maxx-300,maxy-200,300,200);
+				fig2.DibujarVecPuntos(g,2);
+				fig2.windowMap(maxx, maxx-300, maxy, maxy-200, maxx, maxy, g, 2);
+			}
+		};
+		
+		pestañas.add(pestaña1,"Figura 1");
+		pestañas.add(pestaña2,"Figura 2");
+	}
+	
+	//CONSTRUYENDO LA BARRA DE HERRAMIENTAS
+	public void buildToolBar() {
+		toolBar = new JToolBar("",JToolBar.VERTICAL);
+		toolBar.setBackground(uno);
+		toolBar.setFloatable(false);
+		W.add(toolBar,BorderLayout.WEST);
+		
+		URL ruta = getClass().getResource("/Resources/undo.png"); //Botón de restaurar
+		Action A1=new AbstractAction("", new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.restore(1);
+            	else
+            		fig2.restore(2);
+            	pestañas.repaint();
+            }};
+        A1.putValue(Action.SHORT_DESCRIPTION,"Devuelve la figura a su estado original");
+        JButton btnA1 = new JButton(A1);
+        btnA1.setBorder(null);
+        btnA1.setBackground(uno);
+        toolBar.add(btnA1);
+        
+        ruta = getClass().getResource("/Resources/rotate-left.png"); //Botón de girar a la izquierda
+		Action A2=new AbstractAction("", new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.rotateCosHPoint(5,1);
+            	else
+            		fig2.rotateCosHPoint(5,2);
+            	pestañas.repaint();
+            }};
+        A2.putValue(Action.SHORT_DESCRIPTION,"Gira la figura a la izquerda");
+        JButton btnA2 = new JButton(A2);
+        btnA2.setBorder(null);
+        btnA2.setBackground(uno);
+        toolBar.add(btnA2);
+        
+        ruta = getClass().getResource("/Resources/rotate-right.png"); //Botón de girar a al derecha
+		Action A3=new AbstractAction("", new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.rotateSinHPoint(5,1);
+            	else
+            		fig2.rotateSinHPoint(5,2);
+            	pestañas.repaint();
+            }};
+        A3.putValue(Action.SHORT_DESCRIPTION,"Gira la figura a la derecha");
+        JButton btnA3 = new JButton(A3);
+        btnA3.setBorder(null);
+        btnA3.setBackground(uno);
+        toolBar.add(btnA3);
+        
+        ruta = getClass().getResource("/Resources/refx.png"); //Botón de reflejar respecto al eje X
+		Action A4=new AbstractAction("",new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.reflectHPoint(1,-1,1);
+            	else
+            		fig2.reflectHPoint(1,-1,2);
+            	pestañas.repaint();
+            }};
+        A4.putValue(Action.SHORT_DESCRIPTION,"Refleja la figura con respecto al eje X");
+        JButton btnA4 = new JButton(A4);
+        btnA4.setBorder(null);
+        btnA4.setBackground(uno);
+        toolBar.add(btnA4);
+		
+        ruta = getClass().getResource("/Resources/refy.png"); //Botón de reflejar respecto al eje Y
+        Action A5=new AbstractAction("",new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.reflectHPoint(-1,1,1);
+            	else
+            		fig2.reflectHPoint(-1,1,2);
+            	pestañas.repaint();
+            }};
+        A5.putValue(Action.SHORT_DESCRIPTION,"Refleja la figura con respecto al eje Y");
+        JButton btnA5 = new JButton(A5);
+        btnA5.setBorder(null);
+        btnA5.setBackground(uno);
+        toolBar.add(btnA5);
+        
+        ruta = getClass().getResource("/Resources/refxy.png"); //Botón de reflejar respecto a ambos ejes
+        Action A6=new AbstractAction("",new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.reflectHPoint(-1,-1,1);
+            	else
+            		fig2.reflectHPoint(-1,-1,2);
+            	pestañas.repaint();
+            }};
+        A6.putValue(Action.SHORT_DESCRIPTION,"Refleja la figura sobre ambos ejes");
+        JButton btnA6 = new JButton(A6);
+        btnA6.setBorder(null);
+        btnA6.setBackground(uno);
+        toolBar.add(btnA6);
+        
+        ruta = getClass().getResource("/Resources/close.png"); //Botón de aumentar tamaño
+        Action A7=new AbstractAction("",new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.scaleHPoint(1.1,1);
+            	else
+            		fig2.scaleHPoint(1.1,2);
+            	pestañas.repaint();
+            }};
+        A7.putValue(Action.SHORT_DESCRIPTION,"Aumenta el tamaño de la figuras");
+        JButton btnA7 = new JButton(A7);
+        btnA7.setBorder(null);
+        btnA7.setBackground(uno);
+        toolBar.add(btnA7);
+        
+        ruta = getClass().getResource("/Resources/far.png"); //Botón de disminuir tamaño
+        Action A8=new AbstractAction("",new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	if(pestañas.getSelectedIndex() == 0)
+            		fig1.scaleHPoint(0.9,1);
+            	else
+            		fig2.scaleHPoint(0.9,2);
+            	pestañas.repaint();
+            }};
+        A8.putValue(Action.SHORT_DESCRIPTION,"Disminuye el tamaño de la figura");
+        JButton btnA8 = new JButton(A8);
+        btnA8.setBorder(null);
+        btnA8.setBackground(uno);
+        toolBar.add(btnA8);
+        
+        ruta = getClass().getResource("/Resources/exit.png"); //Salir del programa
+        Action A9=new AbstractAction("",new ImageIcon(ruta)){
+            public void actionPerformed(ActionEvent arg0) {
+            	System.exit(0);
+            }};
+        A9.putValue(Action.SHORT_DESCRIPTION,"Salir del programa");
+        JButton btnA9 = new JButton(A9);
+        btnA9.setBorder(null);
+        btnA9.setBackground(uno);
+        toolBar.add(btnA9);
+	}
+	
+	//CONSTRUYENDO EL MENU
+	private void buildMenu() {
+		menuBar = new JMenuBar();
+		menuBar.setBackground(dos);
+		W.setJMenuBar(menuBar);
+		
+		menu1 = new JMenu("Trasformaciones");
+		menu1.setForeground(uno);
+		
+		menu2 = new JMenu("Acerca de");
+		menu2.setForeground(uno);
+		
+		menuBar.add(menu1); menuBar.add(menu2);
+		
+		mOptions = new String[] {"Restaurar","Escalar","Deformar","Rotar","Trasladar",  //Se crea arreglo de nombres de las opciones del menu
+								"Reflejar","Salir","Desarrollador","Ayuda"};		   //para no hacer el
+		menuOptions = new JMenuItem[9];	//Se crea arreglo de pciones
+		
+		for(int i=0;i<mOptions.length;i++) {		//Ciclo para crear cada uno de los item del menú en un ciclo
+			menuOptions[i] = new JMenuItem(mOptions[i]);
+			menuOptions[i].setBackground(uno);
+		}
+		for(int i=0;i<7;i++) {//Ciclo para añadir los items al menu
+			menu1.add(menuOptions[i]);
+		}
+		menu2.add(menuOptions[7]); menu2.add(menuOptions[8]); //Los últimos items se agregan al otro menu
+		
+		//menuOptions[0].addActionListener(this);
+		menuOptions[0].setMnemonic('R');
+		menuOptions[0].setToolTipText("Restaura la figura a su forma originaL");
+		ruta = getClass().getResource("/Resources/undo.png");
+		menuOptions[0].setIcon(new ImageIcon(ruta));
+		menuOptions[0].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R,InputEvent.ALT_MASK));
+		
+		//menuOptions[1].addActionListener(this);
+		menuOptions[1].setMnemonic('E');
+		menuOptions[1].setToolTipText("Cambia el tamaño de la figura");
+		ruta = getClass().getResource("/Resources/scale.png");
+		menuOptions[1].setIcon(new ImageIcon(ruta));
+		menuOptions[1].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z,InputEvent.ALT_MASK));
+		
+		//menuOptions[2].addActionListener(this);
+		menuOptions[2].setMnemonic('D');
+		menuOptions[2].setToolTipText("Cambia la forma de la figura");
+		ruta = getClass().getResource("/Resources/sheary.png");
+		menuOptions[2].setIcon(new ImageIcon(ruta));
+		menuOptions[2].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D,InputEvent.ALT_MASK));
+		
+		//menuOptions[3].addActionListener(this);
+		menuOptions[3].setMnemonic('S');
+		menuOptions[3].setToolTipText("Rota la figura");
+		ruta = getClass().getResource("/Resources/rotate-right.png");
+		menuOptions[3].setIcon(new ImageIcon(ruta));
+		menuOptions[3].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,InputEvent.ALT_MASK));
+		
+		//menuOptions[4].addActionListener(this);
+		menuOptions[4].setMnemonic('T');
+		menuOptions[4].setToolTipText("Traslada la figura a un pundo deseado");
+		ruta = getClass().getResource("/Resources/move.png");
+		menuOptions[4].setIcon(new ImageIcon(ruta));
+		menuOptions[4].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T,InputEvent.ALT_MASK));
+		
+		//menuOptions[5].addActionListener(this);
+		menuOptions[5].setMnemonic('X');
+		menuOptions[5].setToolTipText("Refleja la figura");
+		ruta = getClass().getResource("/Resources/refy.png");
+		menuOptions[5].setIcon(new ImageIcon(ruta));
+		menuOptions[5].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,InputEvent.ALT_MASK));
+		
+		//menuOptions[6].addActionListener(this);
+		menuOptions[6].setMnemonic('Q');
+		menuOptions[6].setToolTipText("Salir del programa");
+		ruta = getClass().getResource("/Resources/exit.png");
+		menuOptions[6].setIcon(new ImageIcon(ruta));
+		menuOptions[6].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,InputEvent.ALT_MASK));
+		
+		menuOptions[7].setMnemonic('A');
+		menuOptions[7].setToolTipText("Muestra la información del desarrollador");
+		ruta = getClass().getResource("/Resources/developer.png");
+		menuOptions[7].setIcon(new ImageIcon(ruta));
+		menuOptions[7].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,InputEvent.ALT_MASK));
+		
+		menuOptions[8].setMnemonic('H');
+		menuOptions[8].setToolTipText("Muestra una ayudar sobre el funcionamiento del programa");
+		ruta = getClass().getResource("/Resources/help.png");
+		menuOptions[8].setIcon(new ImageIcon(ruta));
+		menuOptions[8].setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H,InputEvent.ALT_MASK));
+	}
+	
+	//MÉTODO MAIN PARA INICIAR PROGRAMA
+	public static void main(String[] args) {
+		new MainInterface();
+	}
+}
